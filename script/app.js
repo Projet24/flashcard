58efbd5366399c08fc9125b2a1547d322f18b4ba
vincent-heng <vincentheng@users.noreply.github.com>
2015-03-02 23:58:46 +0100

    Added controller on Flash Card Creation page

--- script/app.js
+++ script/app.js
@@ -26,6 +26,37 @@ app.controller("IndexCtrl", function($scope){
 
 app.controller("NewCardCtrl", function($scope){
 
+	$scope.flashcard = {};
+
+	var flashcard = $scope.flashcard;
+
+	flashcard.name = "";
+	flashcard.words = [];
+
+	var addWord = function(word, translation) {
+		if (word == undefined ) word = "";
+		if (translation == undefined) translation = "";
+		flashcard.words.push({
+			"word" : word,
+			"translation" : translation
+		});
+	}
+	var setFlashcard = function() {
+		addWord("hello", "bonjour");
+		addWord();
+	}
+	setFlashcard();
+
+	flashcard.moreLines = function() {
+		if (flashcard.words[flashcard.words.length-1].word != undefined && flashcard.words[flashcard.words.length-1].word != "") {
+			addWord();
+		}
+	}
+
+	flashcard.save = function() {
+		flashcard.words.pop();
+		console.log(flashcard);
+	}
 });
 
 app.controller("TestCardCtrl", function($scope){
